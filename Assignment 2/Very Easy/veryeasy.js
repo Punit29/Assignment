88059fafa85404dae67520b58dd997faaa10ea49
Punit29 <44257688+Punit29@users.noreply.github.com>
2020-07-19 23:32:59 +0530

    very easy questions

---
+++ Assignment 2/Very Easy/veryeasy.js
@@ -0,0 +1,17 @@
+function minToSec(mins){
+    return mins*60;
+}
+
+console.log("Qustion 1: ");
+console.log(minToSec(3));
+
+
+function divideEven(div,divi){
+    if(div%divi==0){
+        return true;
+    }
+    return false;
+}
+
+console.log("Question 2: ");
+console.log(divideEven(85,4));
